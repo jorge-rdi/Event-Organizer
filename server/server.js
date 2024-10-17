f0e3ec0e03e22a4d50a4f4a047c9590a5980ae99
jorge-rdi <144245705+jorge-rdi@users.noreply.github.com>
2024-10-17 05:57:17 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 app.use(cookieParser());
 require("./config/mongoose.config");
 app.use(
